c738703e8af0f39f9af5beca1cd5dac5c66c3d1c
Aoife Sayers <aoifesayers@gmail.com>
2018-03-08 23:35:18 +0000

    Initial Commit

---
+++ App_Code/Function.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP2
+{
+    //Contains add() & subtract() methods that are Unit tested by NUnit in TestClass.cs
+    public class Function
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ App_Code/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace ASP2
+{
+    //Class that carries out NUnit Tests on Function.cs
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void add()
+        {
+            ASP2.Function f = new ASP2.Function();
+            Assert.AreEqual(f.add(5,5), 10);
+        }
+        [TestCase]
+        public void testSubtract()
+        {
+            ASP2.Function f = new ASP2.Function();
+            Assert.AreEqual(f.subtract(20, 10), 10);
+        }
+    }
+}
