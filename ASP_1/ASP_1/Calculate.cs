82159c14b4be1c5d9d51328b04b748004dc66a65
Aoife Sayers <aoifesayers@gmail.com>
2018-03-12 00:01:36 +0000

    Added Octopack, Nunit and TestCalculate.cs

--- ASP_1/ASP_1/Calculate.cs
+++ ASP_1/ASP_1/Calculate.cs
@@ -3,27 +3,27 @@ namespace ASP_1
 {
     public class Calculate
     {
-        public int Add(int Value1, int Value2)
+        public int add(int Value1, int Value2)
         {
             return Value1 + Value2;
         }
-        public int Subtract(int Value1, int Value2)
+        public int subtract(int Value1, int Value2)
         {
             return Value1 - Value2;
         }
-        public int Multiply(int Value1, int Value2)
+        public int multiply(int Value1, int Value2)
         {
             return Value1 * Value2;
         }
-        public double Divide(int Value1, int Value2)
+        public double divide(int Value1, int Value2)
         {
             return Value1 / Value2;
         }
-        public string Percentage(int Value1, int Value2)
+        public string percentage(int Value1, int Value2)
         {
             Value1 = Value1 * 100;
 
-            return Divide(Value1, Value2) + "%";
+            return divide(Value1, Value2) + "%";
         }
     }
 }
---
+++ ASP_1/ASP_1/TestCalculate.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace ASP2
+{
+    //Class that carries out NUnit Tests on Function.cs
+    [TestFixture]
+    public class TestCalculate
+    {
+        [TestCase]
+        public void testAdd()
+        {
+            ASP_1.Calculate f = new ASP_1.Calculate();
+            Assert.AreEqual(f.add(5, 5), 10);
+            Assert.AreEqual(f.add(-5, -3), - 8);
+            Assert.AreNotEqual(f.add(2, 2), 2);
+        }
+        [TestCase]
+        public void testSubtract()
+        {
+            ASP_1.Calculate f = new ASP_1.Calculate();
+            Assert.AreEqual(f.subtract(5, 5), 0);
+            Assert.AreEqual(f.subtract(-5, -3), -2);
+            Assert.AreNotEqual(f.subtract(2, 2), 2);
+        }
+        [TestCase]
+        public void testMultiply()
+        {
+            ASP_1.Calculate f = new ASP_1.Calculate();
+            Assert.AreEqual(f.multiply(5, 5), 25);
+            Assert.AreEqual(f.multiply(-5, -3), 15);
+            Assert.AreNotEqual(f.multiply(2, 2), 5);
+        }
+
+        [TestCase]
+        public void testDivide()
+        {
+            ASP_1.Calculate f = new ASP_1.Calculate();
+            Assert.AreEqual(f.divide(5, 5), 0);
+            Assert.AreEqual(f.divide(-15, -3), 5);
+            Assert.AreNotEqual(f.divide(2, 2), 2);
+        }
+
+
+    }
+}
